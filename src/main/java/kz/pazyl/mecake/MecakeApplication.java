3adc6641ae8b4c843e503a49490e1b53c0aa214e
Yerbolat <twinspazyl@gmail.com>
2023-01-17 22:06:03 +0600

    init mecake project

---
+++ src/main/java/kz/pazyl/mecake/MecakeApplication.java
@@ -0,0 +1,13 @@
+package kz.pazyl.mecake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MecakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/pazyl/mecake/MecakeApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.pazyl.mecake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
